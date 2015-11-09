faf5720f5b0e5e780e47185aba328ea518305927
Bayram Kiran <kiranbayram@gmail.com>
2015-11-09 18:30:43 +0100

    Added the DOM library.

--- build.sbt
+++ build.sbt
@@ -3,3 +3,5 @@ enablePlugins(ScalaJSPlugin)
 name := "drawings"
 
 scalaVersion := "2.11.7" // or any other Scala version >= 2.10.2
+
+libraryDependencies += "org.scala-js" %%% "scalajs-dom" % "0.8.0"
--- src/main/scala/com/drawing/SampleJSApp.scala
+++ src/main/scala/com/drawing/SampleJSApp.scala
@@ -1,11 +1,28 @@
 package com.drawing
 
+import org.scalajs.dom
+import dom.document
 import scala.scalajs.js._
+import scala.scalajs.js.annotation.JSExport
 
 object SampleJSApp extends JSApp {
   def main(): Unit = {
     println("Hello world! This is a scala js app.")
 
     eval("alert('Alerted')")
+
+    appendPar(document.body, "Hello World")
+  }
+
+  private def appendPar(targetNode: dom.Node, text: String): Unit = {
+    val parNode = document.createElement("p")
+    val textNode = document.createTextNode(text)
+    parNode.appendChild(textNode)
+    targetNode.appendChild(parNode)
+  }
+
+  @JSExport
+  def addClickedMessage(): Unit = {
+    appendPar(document.body, "You clicked the button!")
   }
 }
