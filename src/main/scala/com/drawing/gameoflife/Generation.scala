52bc40648d4c3258ad7a83d3634ff471489c4005
Bayram Kiran <kiranbayram@gmail.com>
2015-11-22 14:20:09 +0100

    Implemented nextGeneration method.

--- src/main/scala/com/drawing/gameoflife/Generation.scala
+++ src/main/scala/com/drawing/gameoflife/Generation.scala
@@ -2,14 +2,35 @@ package com.drawing.gameoflife
 
 import CellStates._
 
-class Generation(board: Matrix) {
+class Generation(matrix: Matrix) {
 
-  private val width = board(0).length
-  private val length = board.length
+  private val width = matrix(0).length
+  private val length = matrix.length
 
-  def cellAt(x: Int, y: Int): Cell = board(x)(y)
+  def cellAt(x: Int, y: Int): Cell = matrix(x)(y)
 
-  def nextGeneration: Generation = ???
+  def nextGeneration: Generation = {
+    val newMatrix: Matrix = Array.ofDim[Cell](width, length)
+
+    for { i <- 0 to width - 1; j <- 0 to length - 1 } {
+      matrix(i)(j) match {
+        case Dead => {
+          aliveNeighbourCount(i, j) match {
+            case 3 => newMatrix(i)(j) = Cell(Alive)
+            case _ => newMatrix(i)(j) = Cell(Dead)
+          }
+        }
+        case Alive => {
+          aliveNeighbourCount(i, j) match {
+            case c if c == 2 || c == 3  => newMatrix(i)(j) = Cell(Alive)
+            case _                      => newMatrix(i)(j) = Cell(Dead)
+          }
+        }
+      }
+    }
+
+    new Generation(newMatrix)
+  }
 
   private def aliveNeighbourCount(x: Int, y: Int): Int = {
     val minX = math.max(0, x - 1)
@@ -26,6 +47,6 @@ class Generation(board: Matrix) {
   }
 
   private def isAlive(x: Int, y: Int): Boolean =
-    board(x)(y).state == Alive
+    matrix(x)(y).state == Alive
 
 }
