9b990369456df98c307924678dbca8d6fd576c1f
Bayram Kiran <kiranbayram@gmail.com>
2015-11-22 20:07:45 +0100

    Added first test case for game of life.

--- build.sbt
+++ build.sbt
@@ -5,3 +5,5 @@ name := "drawings"
 scalaVersion := "2.11.7" // or any other Scala version >= 2.10.2
 
 libraryDependencies += "org.scala-js" %%% "scalajs-dom" % "0.8.0"
+
+libraryDependencies += "org.specs2" %% "specs2" % "2.4"
---
+++ src/test/scala/TestGameOfLife.scala
@@ -0,0 +1,29 @@
+import com.drawing.gameoflife.{Generation, Cell}
+import com.drawing.gameoflife.CellStates._
+import org.specs2.Specification
+
+class TestGameOfLife extends Specification {
+
+  def is = s2"""
+    A dead generation must produce a dead child generation $testDeadGeneration
+    A horizontal-line generation must produce a vertical-line generation $pending
+    A horizontal-line generation result in same after two generations $pending
+  """
+
+  def testDeadGeneration = {
+    val deadMatrix = Array.fill[Cell](2, 3) { Cell(Dead) }
+
+    val deadGeneration = new Generation(deadMatrix)
+
+    val nextGeneration = deadGeneration.nextGeneration
+
+    (nextGeneration.cellAt(0, 0) mustEqual Cell(Dead)) and
+    (nextGeneration.cellAt(0, 1) mustEqual Cell(Dead)) and
+    (nextGeneration.cellAt(0, 2) mustEqual Cell(Dead)) and
+    (nextGeneration.cellAt(1, 0) mustEqual Cell(Dead)) and
+    (nextGeneration.cellAt(1, 1) mustEqual Cell(Dead)) and
+    (nextGeneration.cellAt(1, 2) mustEqual Cell(Alive))
+  }
+
+}
+
